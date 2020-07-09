96e815d4f6eedb25df69c34f92e9fb1cf363ed78
jamesborn89 <jamesborn89@gmail.com>
2020-07-09 19:25:52 +0400

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-  $('.weekly__slaider').slick({
+  $('.weekly__slider').slick({
     prevArrow: '<button class="slick-prev" type="button"><img src="images/slaider/arrow-left.png" alt="prev"></button>',
     nextArrow: '<button class="slick-next" type="button"><img src="images/slaider/arrow-right.png" alt="next"></button>'
 
@@ -10,7 +10,9 @@ $(function () {
     rating: 4.5,
     starWidth: "15px",
     readOnly: true,
-    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z "></path>"+"</svg>'
+    starSvg: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><path d="M0,0h24v24H0V0z" fill="none"/><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><polygon opacity=".3" points="12,15.4 8.24,17.67 9.24,13.39 5.92,10.51 10.3,10.13 12,6.1 13.71,10.14 18.09,10.52 14.77,13.4 15.77,17.68"/><path d="M22,9.24l-7.19-0.62L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27L18.18,21l-1.63-7.03L22,9.24z M12,15.4l-3.76,2.27 l1-4.28l-3.32-2.88l4.38-0.38L12,6.1l1.71,4.04l4.38,0.38l-3.32,2.88l1,4.28L12,15.4z"/></g></g>+"</svg>'
   });
+
+  var mixer = mixitup('.products__items');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,6 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/rateyo/src/jquery.rateyo.css',
-        'node_modules/mixitup/dist/mixitup.js',
         'node_modules/magnific-popup/dist/magnific-popup.css'
 
     ])
@@ -34,6 +33,7 @@ gulp.task('style', function(){
 
 gulp.task('script', function(){
     return gulp.src([
+        'node_modules/mixitup/dist/mixitup.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/rateyo/src/jquery.rateyo.js',
         'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
